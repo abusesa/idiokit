0c77d55230309e35d84b94fe172cd67e9d3aaf99
Joachim Viide <jviide@iki.fi>
2009-10-24 11:03:06 +0000

    Optimizations for the main run loop.

--- callqueue.py
+++ callqueue.py
@@ -2,21 +2,9 @@ from __future__ import with_statement
 import threading
 import contextlib
 
-class Callback(object):
-    def __init__(self, func, *args, **keys):
-        self.func = func
-        self.args = args
-        self.keys = keys
-
-    def __call__(self, *args, **keys):
-        new_args = self.args + args
-        new_keys = dict(self.keys)
-        new_keys.update(keys)
-        return self.func(*new_args, **new_keys)
-
 class CallQueue(object):
     def __init__(self):
-        self.queue_lock = threading.RLock()
+        self.queue_lock = threading.Lock()
         self.exclusive_lock = threading.RLock()
 
         self.queue = list()
@@ -25,23 +13,20 @@ class CallQueue(object):
         self.thread = threading.Thread(target=self._run)
         self.thread.setDaemon(True)
         self.thread.start()
+
+    def _run(self):
+        event = threading.Event()
+        while True:
+            self.iterate(event.set)
+            event.wait()
+            event.clear()
         
     def add(self, func, *args, **keys):
-        call = Callback(func, *args, **keys)
         with self.queue_lock:
-            self.queue.append(call)
+            self.queue.append((func, args, keys))
             if self.callback is not None:
-                callback, self.callback = self.callback, None
-                callback()
-        return call
-
-    def remove(self, call):
-        with self.queue_lock:
-            try:
-                self.queue.remove(call)
-            except ValueError:
-                return False
-        return True
+                self.callback()
+                self.callback = None
 
     @contextlib.contextmanager
     def exclusive(self):
@@ -55,19 +40,10 @@ class CallQueue(object):
                 self.queue = list()
                 self.callback = callback
                 
-            for call in queue:
-                call()
-
-    def _run(self):
-        event = threading.Event()
-
-        while True:
-            self.iterate(event.set)
-            event.wait()
-            event.clear()
+            for func, args, keys in queue:
+                func(*args, **keys)
 global_queue = CallQueue()
 
 iterate = global_queue.iterate
 exclusive = global_queue.exclusive
 add = global_queue.add
-remove = global_queue.remove
