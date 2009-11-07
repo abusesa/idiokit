dca028fbb74754a136801997ee570d2acd662c7c
Joachim Viide <jviide@iki.fi>
2009-11-07 14:03:30 +0000

    Cleanup for the previous commit.

--- callqueue.py
+++ callqueue.py
@@ -5,16 +5,7 @@ from thread import allocate_lock
 class CallQueue(object):
     def __init__(self):
         self.queue_lock = allocate_lock()
-        self.queue_lock_acquire = self.queue_lock.acquire
-        self.queue_lock_release = self.queue_lock.release
         self.exclusive_lock = allocate_lock()
-        self.exclusive_lock_acquire = self.exclusive_lock.acquire
-        self.exclusive_lock_release = self.exclusive_lock.release
-
-        queue_lock_acquire = self.queue_lock.acquire
-        queue_lock_release = self.queue_lock.release
-        exclusive_lock_acquire = self.exclusive_lock.acquire
-        exclusive_lock_release = self.exclusive_lock.release
         
         self.queue = list()
         self.callback = None
@@ -34,36 +25,36 @@ class CallQueue(object):
             iterate(release)
         
     def add(self, func, *args, **keys):
-        self.queue_lock_acquire()
+        self.queue_lock.acquire()
         try:
             self.queue.append((func, args, keys))
             if self.callback is not None:
                 self.callback()
                 self.callback = None
         finally:
-            self.queue_lock_release()
+            self.queue_lock.release()
 
     @contextlib.contextmanager
     def exclusive(self):
-        self.exclusive_lock_acquire()
+        self.exclusive_lock.acquire()
         try:
             yield
         finally:
-            self.exclusive_lock_release()
+            self.exclusive_lock.release()
 
     def iterate(self, callback=None):
-        self.exclusive_lock_acquire()
+        self.exclusive_lock.acquire()
         try:
-            self.queue_lock_acquire()
+            self.queue_lock.acquire()
             queue = self.queue
             self.queue = list()
             self.callback = callback
-            self.queue_lock_release()
+            self.queue_lock.release()
                 
             for func, args, keys in queue:
                 func(*args, **keys)
         finally:
-            self.exclusive_lock_release()
+            self.exclusive_lock.release()
 
 global_queue = CallQueue()
 
