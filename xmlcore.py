32a1dd857a660652b7fdb4d9cdb8a45401fbe9bc
Joachim Viide <jviide@iki.fi>
2009-11-07 17:21:20 +0000

    Removed the weakref dance from xmlcore.py. No idea what I was thinking when I wrote it.

--- xmlcore.py
+++ xmlcore.py
@@ -1,5 +1,4 @@
 import re
-import weakref
 import xml.parsers.expat
 from xml.sax.saxutils import escape, quoteattr
 
@@ -43,36 +42,12 @@ def namespace_split(name):
     return split
 
 class Element(object):
-    element_refs = dict()
-
-    @classmethod
-    def _cleanup(cls, ref):
-        attrs, children = cls.element_refs.pop(ref)
-
-        for child in children:
-            child._lose_parent(attrs)
-
-    def _set_parent(self, parent):
-        self._parent = parent._ref
-
-    def _lose_parent(self, attrs):
-        for key, value in attrs.iteritems():
-            if not key.startswith("xmlns"):
-                continue
-            if key in self.attrs:
-                continue
-            self.attrs[key] = value
-        self._parent = None
-
     def _search(self, attr):
         if attr in self.attrs:
             return self.attrs[attr]
         if self._parent is None:
             return None
-        parent = self._parent()
-        if parent is None:
-            return None
-        return parent._search(attr)
+        return self._parent._search(attr)
 
     def get_ns(self):
         ns_name = "xmlns"
@@ -96,9 +71,6 @@ class Element(object):
         self.text = ""
         self.tail = ""
 
-        self._ref = weakref.ref(self, self._cleanup)
-        self.element_refs[self._ref] = self.attrs, self._children
-
         for key, value in keys.iteritems():
             self.set_attr(key, value)
 
@@ -112,7 +84,7 @@ class Element(object):
 
     def add(self, *children):
         for child in children:
-            child._set_parent(self)
+            child._parent = self
         self._children.extend(children)
 
     def children(self, *args, **keys):
@@ -217,7 +189,6 @@ class ElementParser(object):
         if self.stack[-1].ns != STREAM_NS:
             return
         del self.stack[-1]._children[-1]
-        current._lose_parent(self.stack[-1].attrs)
         self.collected.append(current)
 
     def char_data(self, data):
