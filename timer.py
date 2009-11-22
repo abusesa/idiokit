1ed184a027502bea393007cb5f6a0da5aa433bb3
Joachim Viide <jviide@iki.fi>
2009-11-22 23:37:41 +0000

    Made timer.py use threado's builtin threadpool system.

--- timer.py
+++ timer.py
@@ -1,72 +1,50 @@
-from __future__ import with_statement
-import threading
 import time
 import heapq
-import atexit
 import threado
+import threading
 
-class TimerThread(object):
+class _Timer(threado.GeneratorStream):
     def __init__(self):
-        self.condition = threading.Condition(threading.Lock())
+        threado.GeneratorStream.__init__(self)
         self.heap = list()
-        self.thread = None
-
-        atexit.register(self.stop)
-        
-    def start(self):
-        with self.condition:
-            if self.thread is not None:
-                return
-            self.thread = threading.Thread(target=self.loop)
-            self.thread.setDaemon(True)
-            self.thread.start()
-
-    def stop(self):
-        with self.condition:
-            if self.thread is None:
-                return
-            thread = self.thread
-            self.thread = None
-            self.condition.notify()
-        thread.join()
-
-    def loop(self):
-        with self.condition:
-            while self.thread is threading.currentThread():
-                current_time = time.time()
-
-                while self.heap:
-                    expire_time, channel = self.heap[0]
-                    if expire_time > current_time:
-                        break
-                    heapq.heappop(self.heap)
-                    channel.finish()
-
-                if self.heap:
-                    timeout = max(self.heap[0][0]-current_time, 0.0)
-                else:
-                    timeout = None
-                self.condition.wait(timeout)
-
-    def _wait(self, delay):
-        expire_time = time.time() + delay
-        channel = threado.Channel()
-        item = expire_time, channel
-
-        with self.condition:
-            heapq.heappush(self.heap, item)
-            if self.heap[0] is item:
-                self.condition.notify()
+        self.event = threading.Event()
         self.start()
 
-        return channel
+    def run(self):
+        heap = list()
+
+        while True:
+            if not heap:
+                item = yield self.inner
+                heapq.heappush(heap, item)
+            else:
+                yield
+
+            for item in self.inner:
+                heapq.heappush(heap, item)
+
+            current_time = time.time()
+            while heap and heap[0][0] <= current_time:
+                _, channel = heapq.heappop(heap)
+                channel.finish()
+            if not heap:
+                continue
+
+            timeout = heap[0][0]-current_time
+            yield self.inner.thread(self.event.wait, timeout)
+            self.event.clear()
 
     @threado.stream
     def sleep(inner, self, delay):
-        waiter = self._wait(delay)
-        while not waiter.was_source:
-            yield inner, waiter
-        inner.finish()
-timer_thread = TimerThread()
+        expire_time = time.time() + delay
+        channel = threado.Channel()
 
-sleep = timer_thread.sleep
+        self.send(expire_time, channel)
+        self.event.set()
+
+        while not channel.was_source:
+            yield inner, channel
+        inner.finish()
+global_timer = _Timer()
+
+sleep = global_timer.sleep
