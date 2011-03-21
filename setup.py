55012fbc0330b2c80cab508a7a43cda35cdda6b0
Joachim Viide <jviide@iki.fi>
2011-03-21 14:45:46 +0200

    Add setup.py for idiokit and reorganize the files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="idiokit",
+    version="1.0",
+    author="Clarified Networks",
+    author_email="contact@clarifiednetworks.com",
+    url="https://bitbucket.org/clarifiednetworks/idiokit",
+    packages=["idiokit"]
+    )
