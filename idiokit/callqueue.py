ae8297df517368dfa4507a56b719d2d2af185092
Joachim Viide <jviide@iki.fi>
2011-08-13 10:44:38 +0900

    idiokit/callqueue: A new callqueue implementation, potentially a bit more performant.

--- idiokit/callqueue.py
+++ idiokit/callqueue.py
@@ -1,61 +1,100 @@
-from __future__ import with_statement
+import threading
+import functools
 import contextlib
-from thread import allocate_lock
+
+class CallNode(object):
+    __slots__ = "func", "args", "keys", "next"
+
+    def __init__(self, func, args, keys):
+        self.func = func
+        self.args = args
+        self.keys = keys
+
+        self.next = None
 
 class CallQueue(object):
     def __init__(self):
-        self.exclusive_lock = allocate_lock()
-        self.queue_lock = allocate_lock()
-        
-        self.queue = list()
+        self.exclusive_lock = threading.Lock()
+
+        queue_lock = threading.Lock()
+        self.queue_acquire = queue_lock.acquire
+        self.queue_release = queue_lock.release
+
+        self.head = None
+        self.tail = None
         self.callback = None
+
+    def iterate(self):
+        self.queue_acquire()
+
+        head = self.head
+        self.head = None
+        self.tail = None
+
+        self.queue_release()
+
+        while head is not None:
+            head.func(*head.args, **head.keys)
+            head = head.next
         
     def add(self, func, *args, **keys):
-        self.queue_lock.acquire()
-        try:
-            self.queue.append((func, args, keys))
-            if self.callback is not None:
-                self.callback()
-                self.callback = None
-        finally:
-            self.queue_lock.release()
+        new_tail = CallNode(func, args, keys)
+
+        self.queue_acquire()
+
+        old_tail = self.tail
+        self.tail = new_tail
+
+        if old_tail is not None:
+            old_tail.next = new_tail
+            self.queue_release()
+            return
+
+        self.head = new_tail
+        callback = self.callback
+        self.queue_release()
+
+        if callback is not None:
+            callback()
 
     @contextlib.contextmanager
     def exclusive(self, callback):
-        queue_lock = self.queue_lock
-        acquire = queue_lock.acquire
-        release = queue_lock.release
+        self.exclusive_lock.acquire()
+        try:
+            self.queue_acquire()
+            old_callback = self.callback
+            self.callback = callback
+            tail = self.tail
+            self.queue_release()
 
-        def iterate():
-            acquire()
-            try:
-                queue = self.queue
-                self.queue = list()
-                self.callback = callback
-            finally:
-                release()
-
-            for func, args, keys in queue:
-                func(*args, **keys)
-
-        with self.exclusive_lock:
-            with queue_lock:
-                old_callback = self.callback
-                self.callback = callback
-
-            if callback is not None:
+            if tail is not None:
                 callback()
 
             try:
-                yield iterate
+                yield self.iterate
             finally:
-                with queue_lock:
-                    self.callback = old_callback
+                self.queue_acquire()
+                self.callback = old_callback
+                tail = self.tail
+                self.queue_release()
 
-                if old_callback is not None:
+                if old_callback is not None and tail is not None:
                     old_callback()
+        finally:
+            self.exclusive_lock.release()
+
+    def queued(self, func):
+        add = self.add
+
+        @functools.wraps(func)
+        def _queued(*args, **keys):
+            add(func, *args, **keys)
+
+        return _queued
 
 global_queue = CallQueue()
 
 exclusive = global_queue.exclusive
+iterate = global_queue.iterate
+queued = global_queue.queued
 add = global_queue.add
