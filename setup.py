d51216c7805755754674fa5e067e99874af87412
Joachim Viide <jviide@iki.fi>
2011-10-13 23:32:49 +0300

    setup.py: Clean up the installation directory before installing.

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
+import os
 from distutils.core import setup
+from distutils.dir_util import remove_tree
+from distutils.util import convert_path
+from distutils.command.install import install as _install
+
+class install(_install):
+    def run(self):
+        path = os.path.join(self.install_lib, "idiokit")
+        remove_tree(convert_path(path))
+        return _install.run(self)
 
 setup(
+    cmdclass={"install": install},
     name="idiokit",
     version="1.0",
     author="Clarified Networks",
