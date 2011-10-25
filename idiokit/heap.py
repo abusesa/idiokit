a261f447ae45d80c2f37910665f94de548f9e0ce
Joachim Viide <jviide@iki.fi>
2011-10-25 11:05:54 +0300

    idiokit/heap.py: Separated the heap implementation from idiokit/timer.py to a separate module and added beginnings of unit tests.
    --HG--
    branch : idiokit-3000

---
+++ idiokit/heap.py
@@ -0,0 +1,100 @@
+class HeapError(Exception):
+    pass
+
+class Heap(object):
+    def __init__(self, iterable=()):
+        self._heap = list()
+
+        for value in iterable:
+            self.push(value)
+
+    def _get(self, node):
+        if not self._heap:
+            raise HeapError("empty heap")
+
+        if node is None:
+            node = self._heap[0]
+
+        if self._heap[node._index] is not node:
+            raise HeapError("node not in the heap")
+
+        return node
+
+    def push(self, value):
+        node = _Node(len(self._heap), value)
+        self._heap.append(node)
+        _up(self._heap, node)
+        return node
+
+    def peek(self, node=None):
+        return self._get(node)._value
+
+    def pop(self, node=None):
+        node = self._get(node)
+
+        last = self._heap.pop()
+        if last is not node:
+            self._heap[node._index] = last
+            last._index = node._index
+            _down(self._heap, last)
+        return node._value
+
+    def __nonzero__(self):
+        return not not self._heap
+
+class _Node(object):
+    __slots__ = "_index", "_value"
+
+    def __init__(self, index, value):
+        self._index = index
+        self._value = value
+
+def _swap(array, left, right):
+    array[left._index] = right
+    array[right._index] = left
+    left._index, right._index = right._index, left._index
+    return right, left
+
+def _up(array, node):
+    while node._index > 0:
+        parent = array[(node._index - 1) // 2]
+        if parent._value <= node._value:
+            break
+        _swap(array, node, parent)
+
+def _down(array, node):
+    length = len(array)
+
+    while True:
+        smallest = node
+
+        left_index = 2 * node._index + 1
+        if left_index < length:
+            left = array[left_index]
+            if left._value < smallest._value:
+                smallest = left
+
+        right_index = left_index + 1
+        if right_index < length:
+            right = array[right_index]
+            if right._value < smallest._value:
+                smallest = right
+
+        if node is smallest:
+            break
+
+        _swap(array, node, smallest)
+
+if __name__ == "__main__":
+    import unittest
+
+    class HeapTests(unittest.TestCase):
+        def test_pop(self):
+            h = Heap([0, 1, 2, 3])
+            assert h.pop() == 0
+            assert h.pop() == 1
+            assert h.pop() == 2
+            assert h.pop() == 3
+            self.assertRaises(HeapError, h.pop)
+
+    unittest.main()
--- idiokit/timer.py
+++ idiokit/timer.py
@@ -4,91 +4,7 @@ import time
 import threading
 import functools
 
-from . import idiokit, threadpool, values, callqueue
-
-class Node(object):
-    __slots__ = "index", "value"
-
-    def __init__(self, index, value):
-        self.index = index
-        self.value = value
-
-def _swap(array, left, right):
-    array[left.index] = right
-    array[right.index] = left
-    left.index, right.index = right.index, left.index
-    return right, left
-
-def _up(array, node):
-    while node.index > 0:
-        parent = array[(node.index - 1) // 2]
-        if parent.value <= node.value:
-            break
-        _swap(array, node, parent)
-
-def _down(array, node):
-    length = len(array)
-
-    while True:
-        smallest = node
-
-        left_index = 2 * node.index + 1
-        if left_index < length:
-            left = array[left_index]
-            if left.value < smallest.value:
-                smallest = left
-
-        right_index = left_index + 1
-        if right_index < length:
-            right = array[right_index]
-            if right.value < smallest.value:
-                smallest = right
-
-        if node is smallest:
-            break
-
-        _swap(array, node, smallest)
-
-class HeapError(Exception):
-    pass
-
-class Heap(object):
-    def __init__(self):
-        self._heap = list()
-
-    def _get(self, node):
-        if not self._heap:
-            raise HeapError("empty heap")
-
-        if node is None:
-            node = self._heap[0]
-
-        if self._heap[node.index] is not node:
-            raise HeapError("node not in the heap")
-
-        return node
-
-    def push(self, value):
-        node = Node(len(self._heap), value)
-        self._heap.append(node)
-        _up(self._heap, node)
-        return node
-
-    def peek(self, node=None):
-        return self._get(node).value
-
-    def pop(self, node=None):
-        node = self._get(node)
-
-        last = self._heap.pop()
-        if last is not node:
-            self._heap[node.index] = last
-            last.index = node.index
-            _down(self._heap, last)
-        return node.value
-
-    def __nonzero__(self):
-        return not not self._heap
+from . import idiokit, threadpool, values, callqueue, heap
 
 class TimerValue(values.ValueBase):
     def cancel(self, value=None):
@@ -96,7 +12,7 @@ class TimerValue(values.ValueBase):
 
 class Timer(object):
     def __init__(self):
-        self._heap = Heap()
+        self._heap = heap.Heap()
         self._lock = threading.Lock()
         self._event = threading.Event()
         self._running = False
@@ -130,7 +46,7 @@ class Timer(object):
         with self._lock:
             try:
                 self._heap.pop(node)
-            except HeapError:
+            except heap.HeapError:
                 pass
 
     def set(self, delay, args=None):
