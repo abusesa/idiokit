fd5648c622feabc2a4d2c7d6295a835a6ab33fd7
Joachim Viide <jviide@iki.fi>
2011-12-09 11:24:26 +0200

    idiokit: Export idiokit.Signal.
    --HG--
    branch : idiokit-3000

--- idiokit/__init__.py
+++ idiokit/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-from .idiokit import stream, next, send, pipe, map, consume, stop, main_loop, Event, Proxy, BrokenPipe
+from .idiokit import stream, next, send, pipe, map, consume, stop, main_loop, Event, Proxy, Signal, BrokenPipe
