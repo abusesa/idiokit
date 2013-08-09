694e13bb3c35520a0a98f145f06482715a431d1d
Joachim Viide <jviide@iki.fi>
2013-08-09 22:37:45 +0300

    idiokit._time: Add error handling to Linux code. Use librt for Linux timing.

--- idiokit/_time.py
+++ idiokit/_time.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 
+import os
 import sys
 import time
 import ctypes
 import ctypes.util
 
 
-def load_libc():
-    libc_path = ctypes.util.find_library("libc")
-    libc = ctypes.cdll.LoadLibrary(libc_path)
+def load_lib(name, use_errno=False):
+    libc_path = ctypes.util.find_library(name)
+    libc = ctypes.CDLL(libc_path, use_errno=use_errno)
     return libc
 
 
@@ -37,16 +38,15 @@ class DarwinTime(object):
                 ("denominator", ctypes.c_uint32)
             ]
 
-        libc = load_libc()
-
-        mach_timebase_info = libc.mach_timebase_info
+        lib = load_lib("c")
+        mach_timebase_info = lib.mach_timebase_info
         mach_timebase_info.restype = None
         mach_timebase_info.argtypes = [ctypes.POINTER(mach_timebase_info_t)]
 
         self._timebase = mach_timebase_info_t()
         mach_timebase_info(self._timebase)
 
-        self._mach_absolute_time = libc.mach_absolute_time
+        self._mach_absolute_time = lib.mach_absolute_time
         self._mach_absolute_time.restype = ctypes.c_uint64
         self._mach_absolute_time.argtypes = []
 
@@ -54,8 +54,10 @@ class DarwinTime(object):
 
     def monotonic(self):
         elapsed = self._mach_absolute_time() - self._start
-        ns = elapsed * self._timebase.numerator / self._timebase.denominator
-        return ns * (10 ** -9)
+        timestamp = elapsed * self._timebase.numerator / self._timebase.denominator
+
+        sec, nsec = divmod(timestamp, 10 ** 9)
+        return sec + nsec * (10 ** -9)
 
 
 class LinuxTime(object):
@@ -71,18 +73,22 @@ class LinuxTime(object):
             ]
         self._timespec = timespec
 
-        libc = load_libc()
-        self._clock_gettime = libc.clock_gettime
+        lib = load_lib("rt", use_errno=True)
+        self._clock_gettime = lib.clock_gettime
         self._clock_gettime.restype = ctypes.c_int
         self._clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
 
         res = self._clock_gettime(self.CLOCK_MONOTONIC, ctypes.byref(timespec()))
         if res == -1:
-            raise RuntimeError("can not use CLOCK_MONOTONIC")
+            error = ctypes.get_errno()
+            raise OSError(error, os.strerror(error))
 
     def monotonic(self):
         spec = self._timespec()
-        self._clock_gettime(self.CLOCK_MONOTONIC, ctypes.byref(spec))
+        res = self._clock_gettime(self.CLOCK_MONOTONIC, ctypes.byref(spec))
+        if res == -1:
+            error = ctypes.get_errno()
+            raise OSError(error, os.strerror(error))
         return spec.tv_sec + spec.tv_nsec * (10 ** -9)
 
 
