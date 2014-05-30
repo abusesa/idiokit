463fa89c143cd4493ea3704f177c5aba0ebb2af7
Joachim Viide <jviide@iki.fi>
2014-05-30 13:26:54 +0300

    idiokit.xmpp: Fix SRV logic. RFC 6120 states that the fallback logic shouldn't be applied when the entity (client in this case) receives an answer to the SRV query but fails to establish a connection using the answer data.

--- idiokit/xmpp/_resolve.py
+++ idiokit/xmpp/_resolve.py
@@ -6,21 +6,14 @@ DEFAULT_XMPP_PORT = 5222
 
 
 @idiokit.stream
-def _add_port_and_count(port):
-    count = 0
-
+def _add_port(port):
     while True:
-        try:
-            family, ip = yield idiokit.next()
-        except StopIteration:
-            idiokit.stop(count)
-
+        family, ip = yield idiokit.next()
         yield idiokit.send(family, ip, port)
-        count += 1
 
 
 def _resolve_host(host, port):
-    return dns.host_lookup(host) | _add_port_and_count(port)
+    return dns.host_lookup(host) | _add_port(port)
 
 
 @idiokit.stream
@@ -32,14 +25,14 @@ def resolve(domain, forced_host=None, forced_port=None):
 
     try:
         srv_records = yield dns.srv("_xmpp-client._tcp." + domain)
-    except dns.ResponseError:
+    except (dns.ResponseError, dns.DNSTimeout):
         srv_records = []
 
-    srv_count = 0
-    for srv_record in dns.ordered_srv_records(srv_records):
-        port = srv_record.port if forced_port is None else forced_port
-        srv_count += yield _resolve_host(srv_record.target, port)
-
-    if srv_count == 0:
+    if not srv_records:
         port = DEFAULT_XMPP_PORT if forced_port is None else forced_port
         yield _resolve_host(domain, port)
+        return
+
+    for srv_record in dns.ordered_srv_records(srv_records):
+        port = srv_record.port if forced_port is None else forced_port
+        yield _resolve_host(srv_record.target, port)
