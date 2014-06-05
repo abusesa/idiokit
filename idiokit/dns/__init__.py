f304ac31142d54679fc741bc7cce2fcba7e3cf43
Joachim Viide <jviide@iki.fi>
2014-06-05 14:36:13 +0300

    idiokit.dns: Publish DNSTimeout error.

--- idiokit/dns/__init__.py
+++ idiokit/dns/__init__.py
@@ -1,2 +1,2 @@
-from ._dns import DNSError, ResponseError, Resolver, a, aaaa, cname, srv, ordered_srv_records, txt, ptr, reverse_lookup
+from ._dns import DNSError, DNSTimeout, ResponseError, Resolver, a, aaaa, cname, srv, ordered_srv_records, txt, ptr, reverse_lookup
 from ._hostlookup import host_lookup
