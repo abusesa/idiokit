3ffc0a47335372182588370e369588d6e9d4669d
Joachim Viide <jviide@iki.fi>
2015-01-18 23:21:44 +0200

    idiokit & idiokit.dns: Modify __init__ files to pass linting.

--- idiokit/__init__.py
+++ idiokit/__init__.py
@@ -1,3 +1,34 @@
-from .idiokit import stream, next, send, pipe, map, consume, stop, main_loop, Event, Proxy, Signal, BrokenPipe
+from .idiokit import (
+    stream,
+    next,
+    send,
+    pipe,
+    map,
+    consume,
+    stop,
+    main_loop,
+    Event,
+    Proxy,
+    Signal,
+    BrokenPipe
+)
 from .timer import sleep
 from .threadpool import thread
+
+
+__all__ = [
+    "stream",
+    "next",
+    "send",
+    "pipe",
+    "map",
+    "consume",
+    "stop",
+    "main_loop",
+    "Event",
+    "Proxy",
+    "Signal",
+    "BrokenPipe",
+    "sleep",
+    "thread"
+]
