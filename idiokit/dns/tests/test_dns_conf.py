f4fad9487df190cdd13218c0176e610a11409f95
Joachim Viide <jviide@iki.fi>
2016-01-06 00:58:58 +0200

    Setup dns._conf tests inside the test methods

--- idiokit/dns/tests/test_dns_conf.py
+++ idiokit/dns/tests/test_dns_conf.py
@@ -21,123 +21,126 @@ def tmpfile(*lines):
     tmp = tempfile.NamedTemporaryFile()
     try:
         tmp.writelines(lines)
+        tmp.flush()
         yield tmp.name
     finally:
         tmp.close()
 
 
 class HostsFileTests(unittest.TestCase):
-    def setUp(self):
-        self._hosts = tempfile.NamedTemporaryFile()
-        self._hosts.writelines(
-            ["# Comments are ignored\n",
-             "198.51.100.1 # This line should be ignored\n"
-             "256.256.256.256 invalid-ip # Invalid IP should be ignored\n",
-             "\n",
-             "  198.51.100.126    IPv4.documentation.net.example   \n",
-             "# Hosts file can have tab separator also\n",
-             "2001:DB8::cafe \t IPv6.documentation.net.example\n",
-             "198.51.100.0 incomplete.last.line.net.example"
-             ]
-        )
-        self._hosts.flush()
-
-    def tearDown(self):
-        if self._hosts:
-            self._hosts.close()
+    lines = [
+        "# Comments are ignored\n",
+        "198.51.100.1 # This line should be ignored\n"
+        "256.256.256.256 invalid-ip # Invalid IP should be ignored\n",
+        "\n",
+        "  198.51.100.126    IPv4.documentation.net.example   \n",
+        "# Hosts file can have tab separator also\n",
+        "2001:DB8::cafe \t IPv6.documentation.net.example\n",
+        "198.51.100.0 incomplete.last.line.net.example"
+    ]
 
     def test_hosts_missing_file(self):
         with tmpdir() as path:
             filepath = os.path.join(path, "does-not-exist")
             hosts = _conf.hosts(path=filepath).load()
+
         self.assertEqual(hosts._ips, {})
         self.assertEqual(hosts._names, {})
 
     def test_hosts_empty_file(self):
         with tmpfile() as empty:
             hosts = _conf.hosts(path=empty).load()
+
         self.assertEqual(hosts._ips, {})
         self.assertEqual(hosts._names, {})
 
     def test_hosts_ipv4_to_names(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
-        for name in hosts.ip_to_names("198.51.100.126"):
-            self.assertEqual(name, "ipv4.documentation.net.example")
-            return
-        self.assertTrue(False)
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
+
+        self.assertEqual(
+            list(hosts.ip_to_names("198.51.100.126")),
+            ["ipv4.documentation.net.example"]
+        )
 
     def test_hosts_ipv6_to_names(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
-        for name in hosts.ip_to_names("2001:DB8::cafe"):
-            self.assertEqual(name, "ipv6.documentation.net.example")
-            return
-        self.assertTrue(False)
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
+
+        self.assertEqual(
+            list(hosts.ip_to_names("2001:DB8::cafe")),
+            ["ipv6.documentation.net.example"]
+        )
 
     def test_hosts_name_to_ipv4(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
-        for ip in hosts.name_to_ips("ipv4.documentation.net.EXAMPLE"):
-            self.assertEqual(ip, "198.51.100.126")
-            return
-        self.assertTrue(False)
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
+
+        self.assertEqual(
+            list(hosts.name_to_ips("ipv4.documentation.net.EXAMPLE")),
+            ["198.51.100.126"]
+        )
 
     def test_hosts_name_to_ipv6(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
-        for ip in hosts.name_to_ips("ipv6.documentation.net.EXAMPLE"):
-            self.assertEqual(ip, "2001:db8::cafe")
-            return
-        self.assertTrue(False)
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
+
+        self.assertEqual(
+            list(hosts.name_to_ips("ipv6.documentation.net.EXAMPLE")),
+            ["2001:db8::cafe"]
+        )
 
     def test_hosts_incomplete_last_line(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
-        for ip in hosts.name_to_ips("incomplete.last.line.net.example"):
-            self.assertEqual(ip, "198.51.100.0")
-        for name in hosts.ip_to_names("198.51.100.0"):
-            self.assertEqual(name, "incomplete.last.line.net.example")
-            return
-        self.assertTrue(False)
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
+
+        self.assertEqual(
+            list(hosts.name_to_ips("incomplete.last.line.net.example")),
+            ["198.51.100.0"]
+        )
+        self.assertEqual(
+            list(hosts.ip_to_names("198.51.100.0")),
+            ["incomplete.last.line.net.example"]
+        )
 
     def test_hosts_invalid_ipv4_to_names(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
         self.assertRaises(ValueError, hosts.ip_to_names, "256.256.256.256")
 
     def test_hosts_invalid_ipv6_to_names(self):
-        hosts = _conf.hosts(path=self._hosts.name).load()
+        with tmpfile(*self.lines) as filename:
+            hosts = _conf.hosts(path=filename).load()
         self.assertRaises(ValueError, hosts.ip_to_names, "2001:db8::gg")
 
 
 class ResolvConfFileTests(unittest.TestCase):
-    def setUp(self):
-        self._resolv_conf = tempfile.NamedTemporaryFile()
-        self._resolv_conf.writelines(
-            ["# Comments are ignored\n",
-             "; This comment also ignored\n",
-             "this-is-ignored-also\n",
-             "domain idiokit.test.example\n",
-             "nameserver 192.0.2.11\n",
-             "nameserver 2001:DB8:CAFE::11\n",
-             "nameserver 2001:db8:cafe::11\n",
-             "nameserver dns1.idiokit.test.example\n",
-             ]
-        )
-        self._resolv_conf.flush()
-
-    def tearDown(self):
-        if self._resolv_conf:
-            self._resolv_conf.close()
-
     def test_resolv_conf_missing_file(self):
         with tmpdir() as path:
             filepath = os.path.join(path, "does-not-exist")
             rc = _conf.resolv_conf(path=filepath).load()
+
         self.assertEqual(rc._servers, ())
 
     def test_resolv_conf_empty_file(self):
         with tmpfile() as empty:
             rc = _conf.resolv_conf(path=empty).load()
+
         self.assertEqual(rc._servers, ())
 
     def test_resolv_conf_servers(self):
-        rc = _conf.resolv_conf(path=self._resolv_conf.name).load()
+        with tmpfile(
+            "# Comments are ignored\n",
+            "; This comment also ignored\n",
+            "this-is-ignored-also\n",
+            "domain idiokit.test.example\n",
+            "nameserver 192.0.2.11\n",
+            "nameserver 2001:DB8:CAFE::11\n",
+            "nameserver 2001:db8:cafe::11\n",
+            "nameserver dns1.idiokit.test.example\n",
+        ) as filename:
+            rc = _conf.resolv_conf(path=filename).load()
+
         self.assertEqual(
             rc.servers,
             (('192.0.2.11', 53), ('2001:db8:cafe::11', 53))
