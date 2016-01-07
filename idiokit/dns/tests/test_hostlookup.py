4b41fe45ff4ab0f43f55e08a9b932c5f1653b7cc
Joachim Viide <jviide@iki.fi>
2016-01-07 16:19:11 +0200

    idiokit.dns.tests.test_hostlookup: Simplify test code

--- idiokit/dns/tests/test_hostlookup.py
+++ idiokit/dns/tests/test_hostlookup.py
@@ -8,8 +8,6 @@ from .. import _dns
 
 
 class HostLookupTests(unittest.TestCase):
-    _hosts = None
-
     def setUp(self):
         self._hosts = tempfile.NamedTemporaryFile()
         self._hosts.writelines([
@@ -19,47 +17,35 @@ class HostLookupTests(unittest.TestCase):
         self._hosts.flush()
 
     def tearDown(self):
-        if self._hosts:
-            self._hosts.close()
+        self._hosts.close()
 
     def _host_lookup(self, lookup):
         hl = _hostlookup.HostLookup(hosts_file=self._hosts.name)
-        return hl.host_lookup(lookup)
+        return idiokit.main_loop(hl.host_lookup(lookup))
 
     def test_ipv4_host_lookup_with_ip(self):
         self.assertEqual(
-            [(AF_INET, '198.51.100.126')],
-            idiokit.main_loop(self._host_lookup('198.51.100.126'))
+            [(AF_INET, "198.51.100.126")],
+            self._host_lookup("198.51.100.126")
         )
 
     def test_ipv4_host_lookup_with_name(self):
         self.assertEqual(
-            [(AF_INET, '198.51.100.126')],
-            idiokit.main_loop(self._host_lookup('ipv4.idiokit.example'))
+            [(AF_INET, "198.51.100.126")],
+            self._host_lookup("ipv4.idiokit.example")
         )
 
     def test_ipv6_host_lookup_with_ip(self):
         self.assertEqual(
-            [(AF_INET6, '2001:db8::cafe')],
-            idiokit.main_loop(self._host_lookup('2001:DB8::cafe'))
+            [(AF_INET6, "2001:db8::cafe")],
+            self._host_lookup("2001:DB8::cafe")
         )
 
     def test_ipv6_host_lookup_with_name(self):
         self.assertEqual(
-            [(AF_INET6, '2001:db8::cafe')],
-            idiokit.main_loop(self._host_lookup('ipv6.idiokit.example'))
+            [(AF_INET6, "2001:db8::cafe")],
+            self._host_lookup("ipv6.idiokit.example")
         )
 
     def test_ipv6_host_lookup_with_unknown_name(self):
-        @idiokit.stream
-        def main():
-            try:
-                hl = _hostlookup.HostLookup(hosts_file=self._hosts.name)
-                yield hl.host_lookup('notfound.idiokit.example')
-            except _dns.ResponseError:
-                idiokit.stop()
-            else:
-                self.fail("Hostname should not be found")
-                idiokit.stop()
-
-        idiokit.main_loop(main())
+        self.assertRaises(_dns.ResponseError, self._host_lookup, "notfound.idiokit.example")
